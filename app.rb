ee4c6638b03549bc46a495d4a8341686367aa46f
Arif Gafizov <agafizov@gmail.com>
2019-10-22 16:39:42 +0300

    Add parameter validation

--- app.rb
+++ app.rb
@@ -11,12 +11,14 @@ def init_db
 end
 
 before do
+	# инициализация БД
 	init_db
 end
 
 configure do
 	# добавили init_db т.к. метод before не исполняется при конфигурации
 	init_db
+	# создает таблицу если она не существует
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts (
     id           INTEGER PRIMARY KEY AUTOINCREMENT,
     created_date DATE,
@@ -28,11 +30,20 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+# обработка гет запроса
 get '/new' do
   erb :new
 end
 
+# обработка пост запроса
 post '/new' do
+	# получаем переменную из пост запроса
   content = params[:content]
+
+  if content.length <= 0
+  	@error = 'Type post text'
+  	return erb :new
+  end
+
   erb "You typed #{content}"
 end
